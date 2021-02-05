5edf89a23cd14a0e37877b1dd4457bc692dead7a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-04 18:49:11 -0600

    Hero completed

---
+++ scripts/AutoScrolling.controller.js
@@ -0,0 +1,20 @@
+function autoScroll() {
+	var div = document.querySelector('.stats')
+	div.style.display = ''
+	var top = div.offsetTop
+	if (window.scrollTop != top) window.scrollTo(0, top)
+}
+function loadAutoScroll() {
+	autoScroll()
+	window.onload = null
+	return false
+}
+function scrollAutoScroll() {
+	autoScroll()
+	window.setTimeout(function () {
+		window.onscroll = null
+	}, 100)
+	return false
+}
+window.onload = loadAutoScroll
+window.onscroll = scrollAutoScroll
